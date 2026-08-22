bb9677aed4c4a6c30a29d48488e66497aea7db68
Kevin Blaue <kevin@blaue-online.com>
2026-08-22 12:35:21 +0200

    refactor: orchestrate stacks in CDK entrypoint

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import { App } from '../lib/constructs/app';
-import { createStacks } from '../lib/stacks';
+import { DatabaseStack } from '../lib/database';
+import { IamRoleGitHubStack } from '../lib/iamRoleGitHub';
+import { WalletApiStack } from '../lib/walletApi';
 
 const app = new App();
-createStacks(app);
+
+if (app.iamGitHubEnabled) {
+  new IamRoleGitHubStack(app, 'iamGitHub');
+}
+
+const database = new DatabaseStack(app, 'database');
+const walletApi = new WalletApiStack(app, 'walletApi', {
+  cardsTable: database.cardsTable,
+});
+walletApi.addStackDependency(database);
